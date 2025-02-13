c9db141dc64fb581e15e5d37a147248eb7cb715b
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2025-02-13 16:12:33 +0500

    font problem solved

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'cdn.sanity.io',
-          },
-        ],
+  images: {
+    domains: ["fonts.gstatic.com", "cdn.sanity.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
       },
+      {
+        protocol: "https",
+        hostname: "fonts.gstatic.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
